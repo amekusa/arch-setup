0986e8522ba751939264f9a6b8b2ae9957b43caf
amekusa <hello@amekusa.com>
2022-10-12 18:36:37 +0900

    [x] Optimize code

--- setup.sh
+++ setup.sh
@@ -338,20 +338,20 @@ if $GUI; then
 		_install xorg-server xorg-xinit || x
 		if [ -n "$GUI_X_KEYMAP" ]; then
 			keymap=($GUI_X_KEYMAP)
+			opts=(XkbLayout XkbModel XkbVariant XkbOptions)
 			temp="$(mktemp)" || x
 			cat <<-EOF > "$temp"
 			Section "InputClass"
 			        Identifier "system-keyboard"
 			        MatchIsKeyboard "on"
 			EOF
-			indent="        "
-			[ -z "${keymap[0]}" ] || echo "${indent}Option \"XkbLayout\" \"${keymap[0]}\"" >> "$temp"
-			[ -z "${keymap[1]}" ] || echo "${indent}Option \"XkbModel\" \"${keymap[1]}\"" >> "$temp"
-			[ -z "${keymap[2]}" ] || echo "${indent}Option \"XkbVariant\" \"${keymap[2]}\"" >> "$temp"
-			[ -z "${keymap[3]}" ] || echo "${indent}Option \"XkbOptions\" \"${keymap[3]}\"" >> "$temp"
+			for i in "${!keymap[@]}"; do
+				echo "        Option \"${opts[$i]}\" \"${keymap[$i]}\"" >> "$temp"
+			done
 			echo "EndSection" >> "$temp"
 			file="/etc/X11/xorg.conf.d/00-keyboard.conf"
 			cat "$temp" > "$file" || x "failed to write: $file"
+			_show "$file"
 			rm "$temp"
 		fi
 	ksat; fi

362ed537624ae4efc33b437d476b743cef688a18
Satoshi Soma <noreply@amekusa.com>
2025-11-28 01:23:49 +0900

    refactor

--- setup.sh
+++ setup.sh
@@ -1,154 +1,129 @@
 #!/bin/bash
-#
-#  Arch Setup
-# --------------- -  *
-#  author: Satoshi Soma (https://amekusa.com)
+# --- Arch Setup ---
+# @author Satoshi Soma (github.com/amekusa)
 # ============================================
 
-EXEC="$(realpath "$0")"
-BASE="$(dirname "$EXEC")"
-CONF="$BASE/setup"
+BASE="$(dirname "$(realpath "$0")")"
+
+CONF="$BASE/conf"
 ASSETS="$BASE/assets"
 BACKUP="$BASE/backup"
 PATCHES="$BASE/patches"
 
-LABEL="arch-setup"
-
-OPT_TASKS=()
-OPT_UPGRADE=true
-OPT_PROMPT=false
-OPT_LIST=false
-
 git submodule init
 git submodule update
 
-. "$BASE/shlib/util.sh"
-. "$BASE/shlib/io.sh"
+. "$BASE/ush/load" util
+. "$BASE/ush/load" io
+. "$BASE/ush/load" task
 
 . "$BASE/lib/util.sh"
-. "$BASE/lib/task.sh"
 
+
+# === PARSE ARGS ===
 _help() {
-	cat << EOF
-Usage:
-  setup.sh [options]
-  setup.sh [options] <task1> <task2> ...
+	cat <<- EOF
+	Usage:
+	  setup.sh [options]
+	  setup.sh [options] <task1> <task2> ...
 
-Options:
-  -h, --help   : Show this text
-  -l, --list   : List task names
-  -p, --prompt : Run in prompt mode
-  --no-upgrade : Skip system upgrade
+	Options:
+	  -h, --help   : Show this text
+	  -l, --list   : List task names
+	  -p, --prompt : Run in prompt mode
+	  -r, --reset  : Reset tasks
+	  -F, --force  : Force run tasks
 
-EOF
+	EOF
 }
 
-# commandline args
+TASK_OPTS=()
 while true; do
 	case "$1" in
 	-h|--help)
 		_help; exit
 		;;
-	-l|--list)
-		OPT_LIST=true
-		;;
-	-p|--prompt)
-		OPT_PROMPT=true
-		;;
-	--no-upgrade)
-		OPT_UPGRADE=false
-		;;
-	-*)
-		echo "invalid option '$1'";
-		echo
-		_help; exit
-		;;
  	*)
-		OPT_TASKS+=("$1")
+		TASK_OPTS+=("$1")
 		;;
 	esac
 	shift || break
 done
 
-# configuration
+
+# === CONFIG ===
 . "$CONF.conf"
-if [ -f "$CONF.local" ]; then . "$CONF.local"
+if [ -f "$CONF.local" ]; then
+	. "$CONF.local"
 else
 	cat <<- EOF > "$CONF.local"
-	##
-	#  setup.local
-	# ----------------- -
+	#  --- setup.local ---
 	#  Edit this file so it suits your needs.
 	#  After save it, run setup.sh again
 	# ========================================
 
 	EOF
 	cat "$CONF.conf" >> "$CONF.local"
-
-	[ -n "$EDITOR" ] || EDITOR="$(_fb-cmd nano micro nvim vim vi)" || _err "editor not found"
+	[ -n "$EDITOR" ] || EDITOR="$(_fb-cmd -f nano nvim vim vi '')" || _die "editor not found"
 	"$EDITOR" "$CONF.local"
 	exit
 fi
 
-if ! $OPT_LIST; then
-	# only root user is allowed
-	_chk-user root
 
-	# upgrade the system first
-	if $OPT_UPGRADE; then
-		_install archlinux-keyring
-		pacman --noconfirm --needed -Syu
-	fi
+# === TASKS ===
+_task-system -s "$BASE/tasks" "${TASK_OPTS[@]}"
+
+# system update
+if _task UPDATE; then
+	_install archlinux-keyring
+	pacman --noconfirm --needed -Syu
 fi
 
-
-# ---- tasks -------- *
-
 # hosts
-if task HOSTS; then
-	_backup "/etc/hosts" || x
+if _task HOSTS; then
+	_backup "/etc/hosts" || _fail
 	cat <<- EOF > "/etc/hosts"
 	127.0.0.1  localhost
 	::1        localhost
 	EOF
-ksat; fi
+fi
 
 # hostname
-if [ -n "$HOSTNAME" ] && task HOSTNAME -d HOSTS; then
+if [ -n "$HOSTNAME" ] && _task HOSTNAME -d HOSTS; then
 	_var HOSTNAME
-	_backup "/etc/hostname" || x
-	echo "$HOSTNAME" > "/etc/hostname" || x
-	echo "127.0.1.1  $HOSTNAME" >> "/etc/hosts" || x
+	_backup "/etc/hostname" || _fail
+	echo "$HOSTNAME" > "/etc/hostname" || _fail
+	echo "127.0.1.1  $HOSTNAME" >> "/etc/hosts" || _fail
 	_show "/etc/hosts"
-ksat; fi
+fi
 
 # locale
-if [ -n "$LOCALE" ] && task LOCALE; then
+if [ -n "$LOCALE" ] && _task LOCALE; then
 	_var LOCALE
-	_uncomment "$LOCALE" "/etc/locale.gen" || x
-	locale-gen || x
-	_save-var LANG "$LOCALE" "/etc/locale.conf" || x
-ksat; fi
+	_uncomment "$LOCALE" "/etc/locale.gen" || _fail
+	locale-gen || _fail
+	_save-var LANG "$LOCALE" "/etc/locale.conf" || _fail
+fi
 
 # timezone
-if [ -n "$TIMEZONE" ] && task TIMEZONE; then
+if [ -n "$TIMEZONE" ] && _task TIMEZONE; then
 	_var TIMEZONE
-	_symlink "/usr/share/zoneinfo/$TIMEZONE" "/etc/localtime" || x
-	hwclock --systohc --utc || x
-ksat; fi
+	_symlink "/usr/share/zoneinfo/$TIMEZONE" "/etc/localtime" || _fail
+	hwclock --systohc --utc || _fail
+fi
 
 # keymap
-if [ -n "$KEYMAP" ] && task KEYMAP; then
+if [ -n "$KEYMAP" ] && _task KEYMAP; then
 	_var KEYMAP
-	loadkeys "$KEYMAP" || x
-	_save-var KEYMAP "$KEYMAP" "/etc/vconsole.conf" || x
-ksat; fi
+	loadkeys "$KEYMAP" || _fail
+	_save-var KEYMAP "$KEYMAP" "/etc/vconsole.conf" || _fail
+fi
 
 # reflector
-if $REFLECTOR && task REFLECTOR; then
-	_install reflector || x
+if $REFLECTOR && _task REFLECTOR; then
+	_install reflector || _fail
 	file="/etc/xdg/reflector/reflector.conf"
-	[ -f "$file" ] || x "file not found: $file"
+	[ -f "$file" ] || _fail "file not found: $file"
 	if [ -n "$REF_COUNTRY" ]; then
 		sed -ri "/^--country /d" "$file"
 		sed -ri "/^# --country /a --country '$REF_COUNTRY'" "$file"
@@ -160,186 +135,187 @@ if $REFLECTOR && task REFLECTOR; then
 		sed -ri "s/^--sort .*/--sort $REF_SORT/" "$file"
 	fi
 	_show "$file"
-	_backup "/etc/pacman.d/mirrorlist" || x
-	reflector "@$file" || x
-	_sys-enable reflector.timer || x
-ksat; fi
+	_backup "/etc/pacman.d/mirrorlist" || _fail
+	reflector "@$file" || _fail
+	_sys-enable reflector.timer || _fail
+fi
 
 # bootloader
-if [ -n "$BOOTLOADER" ] && task BOOTLOADER; then
-	disk="$(_fb "$DISK" $(_disk))" || x "disk not found"
+if [ -n "$BOOTLOADER" ] && _task BOOTLOADER; then
+	disk="$(_fb "$DISK" $(_disk))" || _fail "disk not found"
 	case "$BOOTLOADER" in
 	grub)
-		_install grub || x
-		grub-install --recheck "$disk" || x "cmd failed: grub-install"
-		grub-mkconfig -o "/boot/grub/grub.cfg" || x "cmd failed: grub-mkconfig"
+		_install grub || _fail
+		grub-install --recheck "$disk" || _fail "cmd failed: grub-install"
+		grub-mkconfig -o "/boot/grub/grub.cfg" || _fail "cmd failed: grub-mkconfig"
 		;;
 	*)
-		x "invalid $(_var BOOTLOADER)"
+		_fail "invalid $(_var BOOTLOADER)"
 		;;
 	esac
-ksat; fi
+fi
 
 # root user
-if task ROOT; then
+if _task ROOT; then
 	until passwd; do
 		echo
 		echo "input password for root user"
 	done
-ksat; fi
+fi
 
 # admin user
-if [ -n "$ADMIN" ] && task ADMIN; then
+if [ -n "$ADMIN" ] && _task ADMIN; then
 	_var ADMIN
-	shell="$(_require $ADMIN_SHELL)" || x
+	shell="$(_require $ADMIN_SHELL)" || _fail
 	if _user-exists "$ADMIN"; then
-		usermod -G "$ADMIN_GROUPS" -s "$shell" "$ADMIN" || x "cannot change shell for user: $ADMIN"
+		usermod -G "$ADMIN_GROUPS" -s "$shell" "$ADMIN" || _fail "cannot change shell for user: $ADMIN"
 	else
-		useradd -m -G "$ADMIN_GROUPS" -s "$shell" "$ADMIN" || x "cannot add user: $ADMIN"
+		useradd -m -G "$ADMIN_GROUPS" -s "$shell" "$ADMIN" || _fail "cannot add user: $ADMIN"
 		until passwd "$ADMIN"; do
 			echo
 			echo "input password for user: $ADMIN"
 		done
 	fi
-ksat; fi
+fi
 
 # sudo
-if $SUDO && task SUDO; then
-	_install sudo || x
+if $SUDO && _task SUDO; then
+	_install sudo || _fail
 	file="/etc/sudoers"
 	case "$SUDO_ALLOW" in
-	wheel)      line='%wheel ALL=(ALL:ALL) ALL' ;;
-	wheel-nopw) line='%wheel ALL=(ALL:ALL) NOPASSWD: ALL' ;;
-	sudo)       line='%sudo ALL=(ALL:ALL) ALL' ;;
-	sudo-nopw)  line='%sudo ALL=(ALL:ALL) NOPASSWD: ALL' ;;
-	*) x "invalid $(_var SUDO_ALLOW)" ;;
+		wheel)      line='%wheel ALL=(ALL:ALL) ALL' ;;
+		wheel-nopw) line='%wheel ALL=(ALL:ALL) NOPASSWD: ALL' ;;
+		sudo)       line='%sudo ALL=(ALL:ALL) ALL' ;;
+		sudo-nopw)  line='%sudo ALL=(ALL:ALL) NOPASSWD: ALL' ;;
+		*)          _fail "invalid $(_var SUDO_ALLOW)" ;;
 	esac
-	echo "$line" | _section "$LABEL" "$file" || x "failed to write: $file"
-ksat; fi
+	echo "$line" | _section "$LABEL" "$file" || _fail "failed to write: $file"
+fi
 
 # network
-if [ -n "$NET_MANAGER" ] && task NETWORK; then
+if [ -n "$NET_MANAGER" ] && _task NETWORK; then
 	_var NET_MANAGER
 	case "$NET_MANAGER" in
 	systemd)
 		if $NET_WIRED; then
 			file="wired.network"
-			nif="$(_fb "$NET_INTERFACE" $(_nif "en"))" || x "network interface not found"
+			nif="$(_fb "$NET_INTERFACE" $(_nif "en"))" || _fail "network interface not found"
 		else
 			file="wireless.network"
-			nif="$(_fb "$NET_INTERFACE" $(_nif "wl"))" || x "network interface not found"
+			nif="$(_fb "$NET_INTERFACE" $(_nif "wl"))" || _fail "network interface not found"
 		fi
-		cat "$ASSETS/$file" | _subst "name=$nif" "dhcp=$(_yn $NET_DHCP)" "vm=$(_yn $VM)" > "/etc/systemd/network/$file" || x
+		cat "$ASSETS/$file" | _subst "name=$nif" "dhcp=$(_yn $NET_DHCP)" "vm=$(_yn $VM)" > "/etc/systemd/network/$file" || _fail
 		_show "/etc/systemd/network/$file"
-		_sys-enable systemd-networkd.service || x
-		_sys-enable systemd-resolved.service || x
+		_sys-enable systemd-networkd.service || _fail
+		_sys-enable systemd-resolved.service || _fail
 		;;
 	netctl)
 		# TODO
 		;;
 	nm|networkmanager|NetworkManager)
-		_install networkmanager || x
-		_sys-enable NetworkManager.service || x
-		_sys-enable systemd-resolved.service || x
+		_install networkmanager || _fail
+		_sys-enable NetworkManager.service || _fail
+		_sys-enable systemd-resolved.service || _fail
 		;;
 	*)
-		x "invalid $(_var NETWORK)"
+		_fail "invalid $(_var NETWORK)"
 		;;
 	esac
-ksat; fi
+fi
 
 # ssh authorized keys
-if [ -n "$SSH_AUTH_KEYS" ] && task SSH_AUTH_KEYS -d ADMIN; then
-	_install openssh || x
+if [ -n "$SSH_AUTH_KEYS" ] && _task SSH_AUTH_KEYS -d ADMIN; then
+	_install openssh || _fail
 	dir="/home/$ADMIN/.ssh"
 	file="$dir/authorized_keys"
-	_dir  "$dir"  -m 700 -o "$ADMIN:$ADMIN" || x
-	_file "$file" -m 600 -o "$ADMIN:$ADMIN" || x
+	_dir  "$dir"  -m 700 -o "$ADMIN:$ADMIN" || _fail
+	_file "$file" -m 600 -o "$ADMIN:$ADMIN" || _fail
 	echo "fetching auth keys from: $SSH_AUTH_KEYS ..."
-	curl "$SSH_AUTH_KEYS" > "$file" || x
-ksat; fi
+	curl "$SSH_AUTH_KEYS" > "$file" || _fail
+fi
 
 # ssh server
-if $SSHD && task SSHD -d ADMIN; then
-	_install openssh || x
+if $SSHD && _task SSHD -d ADMIN; then
+	_install openssh || _fail
 	file="/etc/ssh/sshd_config"
-	_backup "$file" || x
+	_backup "$file" || _fail
 	cat "$ASSETS/sshd_config" | _subst \
 		"admin=$ADMIN" \
 		"keepAliveTcp=$(_yn $SSHD_KEEPALIVE_TCP)" \
 		"keepAliveIntvl=$SSHD_KEEPALIVE_INTVL" \
 		"keepAliveCount=$SSHD_KEEPALIVE_COUNT" |
-		_section "$LABEL" "$file" || x "failed to write: $file"
+		_section "$LABEL" "$file" || _fail "failed to write: $file"
 
 	_show "$file"
-	_sys-enable sshd.service || x
-ksat; fi
+	_sys-enable sshd.service || _fail
+fi
 
 # rootkit hunter
-if $RKHUNTER && task RKHUNTER; then
-	exec="$(_require rkhunter)" || x
-	cp "$ASSETS/rkhunter.conf.local" /etc/ || x
+if $RKHUNTER && _task RKHUNTER; then
+	exec="$(_require rkhunter)" || _fail
+	cp "$ASSETS/rkhunter.conf.local" /etc/ || _fail
 
 	file="/etc/systemd/system/rkhunter.service"
-	cat "$ASSETS/rkhunter.service" | _subst "rkhunter=$exec" > "$file" || x "failed to write: $file"
+	cat "$ASSETS/rkhunter.service" | _subst "rkhunter=$exec" > "$file" || _fail "failed to write: $file"
 	_show "$file"
 
 	if [ -n "$RKH_TIMER" ]; then
 		file="/etc/systemd/system/rkhunter.timer"
-		cat "$ASSETS/rkhunter.timer" | _subst "timer=$RKH_TIMER" > "$file" || x "failed to write: $file"
+		cat "$ASSETS/rkhunter.timer" | _subst "timer=$RKH_TIMER" > "$file" || _fail "failed to write: $file"
 		_show "$file"
-		_sys-enable rkhunter.timer || x
+		_sys-enable rkhunter.timer || _fail
 	fi
-ksat; fi
+fi
 
 # paccache
-if $PACCACHE && task PACCACHE; then
-	_install pacman-contrib || x
-	_sys-enable paccache.timer || x
-ksat; fi
+if $PACCACHE && _task PACCACHE; then
+	_install pacman-contrib || _fail
+	_sys-enable paccache.timer || _fail
+fi
 
 # git
-if [ -n "$GIT_EMAIL" ] && [ -n "$GIT_NAME" ] && task GIT -d ADMIN; then
-	_install git || x
+if [ -n "$GIT_EMAIL" ] && [ -n "$GIT_NAME" ] && _task GIT -d ADMIN; then
+	_install git || _fail
 	_var GIT_EMAIL
 	_var GIT_NAME
 
 	file="$HOME/.gitconfig"
-	cat "$ASSETS/user.gitconfig" | _subst "email=$GIT_EMAIL" "name=$GIT_NAME" > "$file" || x "failed to write: $file"
+	cat "$ASSETS/user.gitconfig" | _subst "email=$GIT_EMAIL" "name=$GIT_NAME" > "$file" || _fail "failed to write: $file"
 	_show "$file"
 
 	copy="/home/$ADMIN/.gitconfig"
-	cp "$file" "$copy" || x "failed to copy: $file -> $copy"
-	chown $ADMIN:$ADMIN "$copy" || x "cmd failed: chown"
-ksat; fi
+	cp "$file" "$copy" || _fail "failed to copy: $file -> $copy"
+	chown $ADMIN:$ADMIN "$copy" || _fail "cmd failed: chown"
+fi
 
 # aur helper
-if $AUR && [ -n "$AUR_HELPER" ] && task AUR_HELPER -d ADMIN SUDO GIT; then
+if $AUR && [ -n "$AUR_HELPER" ] && _task AUR_HELPER -d ADMIN SUDO GIT; then
 	_var AUR_HELPER
 	case "$AUR_HELPER" in
 	yay)
-		sudo -Hu "$ADMIN" bash <<- EOF || x "cannot install: yay"
+		sudo -Hu "$ADMIN" bash <<- EOF || _fail "cannot install: yay"
 		git clone "https://aur.archlinux.org/yay.git" "\$HOME/yay" &&
 		cd "\$HOME/yay" && makepkg -sic --noconfirm --needed
 		rm -rf "\$HOME/yay"
 		EOF
 		;;
 	*)
-		x "invalid $(_var AUR_HELPER)"
+		_fail "invalid $(_var AUR_HELPER)"
 		;;
 	esac
 	cd "$BASE"
-ksat; fi
+fi
 
 # graphical user interface
 if $GUI; then
+
 	# x11
-	if $GUI_X && task GUI_X; then
-		_install xorg-server xorg-xinit || x
+	if $GUI_X && _task GUI_X; then
+		_install xorg-server xorg-xinit || _fail
 		if [ -n "$GUI_X_KEYMAP" ]; then
 			keymap=($GUI_X_KEYMAP)
 			opts=(XkbLayout XkbModel XkbVariant XkbOptions)
-			temp="$(mktemp)" || x
+			temp="$(mktemp)" || _fail
 			cat <<- EOF > "$temp"
 			Section "InputClass"
 			        Identifier "system-keyboard"
@@ -350,114 +326,115 @@ if $GUI; then
 			done
 			echo "EndSection" >> "$temp"
 			file="/etc/X11/xorg.conf.d/00-keyboard.conf"
-			cat "$temp" > "$file" || x "failed to write: $file"
+			cat "$temp" > "$file" || _fail "failed to write: $file"
 			_show "$file"
 			rm "$temp"
 		fi
-	ksat; fi
+	fi
 
 	# gnome
-	if $GUI_GNOME && task GUI_GNOME; then
-		_install gnome || x
-		_sys-enable gdm.service || x
-	ksat; fi
+	if $GUI_GNOME && _task GUI_GNOME; then
+		_install gnome || _fail
+		_sys-enable gdm.service || _fail
+	fi
 
 	# install optional GUI packages
-	if [ -n "$GUI_PKGS" ] && task GUI_PKGS; then
-		_install "${GUI_PKGS[@]}" || x
-	ksat; fi
+	if [ -n "$GUI_PKGS" ] && _task GUI_PKGS; then
+		_install "${GUI_PKGS[@]}" || _fail
+	fi
 fi
 
 # virtual machine
-if $VM && task VM; then
+if $VM && _task VM; then
 	case "$VM_TYPE" in
 	vbox)
-		_install virtualbox-guest-utils || x
-		_sys-enable vboxservice.service || x
+		_install virtualbox-guest-utils || _fail
+		_sys-enable vboxservice.service || _fail
 		;;
 	*)
-		x "invalid $(_var VM_TYPE)"
+		_fail "invalid $(_var VM_TYPE)"
 		;;
 	esac
-ksat; fi
+fi
 
 # install optional packages
-if [ -n "$PKGS" ] && task PKGS; then
-	_install "${PKGS[@]}" || x
-ksat; fi
+if [ -n "$PKGS" ] && _task PKGS; then
+	_install "${PKGS[@]}" || _fail
+fi
 
 # install AUR packages
-if $AUR && [ -n "$AUR_PKGS" ] && task AUR_PKGS -d AUR_HELPER; then
-	_aur-install "${AUR_PKGS[@]}" || x
-ksat; fi
+if $AUR && [ -n "$AUR_PKGS" ] && _task AUR_PKGS -d AUR_HELPER; then
+	_aur-install "${AUR_PKGS[@]}" || _fail
+fi
 
 # patch egrep
-if $PATCH_EGREP && task PATCH_EGREP; then
+if $PATCH_EGREP && _task PATCH_EGREP; then
 	if file="$(which egrep)"; then
-		_backup "$file" || x
-		patch "$file" < "$PATCHES/egrep.patch" || x "cannot patch: $file"
+		_backup "$file" || _fail
+		patch "$file" < "$PATCHES/egrep.patch" || _fail "cannot patch: $file"
 	fi
-ksat; fi
+fi
 
 # patch fgrep
-if $PATCH_FGREP && task PATCH_FGREP; then
+if $PATCH_FGREP && _task PATCH_FGREP; then
 	if file="$(which fgrep)"; then
-		_backup "$file" || x
-		patch "$file" < "$PATCHES/fgrep.patch" || x "cannot patch: $file"
+		_backup "$file" || _fail
+		patch "$file" < "$PATCHES/fgrep.patch" || _fail "cannot patch: $file"
 	fi
-ksat; fi
+fi
 
 # patch rkhunter
-if $PATCH_RKHUNTER && task PATCH_RKHUNTER -d RKHUNTER; then
+if $PATCH_RKHUNTER && _task PATCH_RKHUNTER -d RKHUNTER; then
 	if file="$(which rkhunter)"; then
 		ver="$(_ver rkhunter)"
 		patch="$PATCHES/rkhunter.$ver.patch"
 		if [ -f "$patch" ]; then
-			_backup "$file" || x
-			patch "$file" < "$patch" || x "cannot patch: $file"
+			_backup "$file" || _fail
+			patch "$file" < "$patch" || _fail "cannot patch: $file"
 		fi
 	fi
-ksat; fi
+fi
 
 # etckeeper
-if $ETCKEEPER && task ETCKEEPER -d GIT; then
-	_install etckeeper || x
+if $ETCKEEPER && _task ETCKEEPER -d GIT; then
+	_install etckeeper || _fail
 	file="/etc/.gitignore"
-	cp "$ASSETS/etc.gitignore" "$file" || x "failed to write: $file"
-	etckeeper init || x "cmd failed: etckeeper init"
-ksat; fi
+	cp "$ASSETS/etc.gitignore" "$file" || _fail "failed to write: $file"
+	etckeeper init || _fail "cmd failed: etckeeper init"
+fi
 
 # pacman hooks for rkhunter
-if $RKHUNTER && $RKH_HOOKS && task RKH_HOOKS -d RKHUNTER; then
+if $RKHUNTER && $RKH_HOOKS && _task RKH_HOOKS -d RKHUNTER; then
 	dir="/etc/pacman.d/hooks"
-	[ -d "$dir" ] || mkdir "$dir" || x "cannot create dir: $dir"
+	[ -d "$dir" ] || mkdir "$dir" || _fail "cannot create dir: $dir"
 
 	file="$dir/rkhunter-propupd.hook"
-	cat "$ASSETS/rkhunter-propupd.hook" | _subst "rkhunter=$(which rkhunter)" > "$file" || x "failed to write: $file"
+	cat "$ASSETS/rkhunter-propupd.hook" | _subst "rkhunter=$(which rkhunter)" > "$file" || _fail "failed to write: $file"
 	_show "$file"
 
 	file="$dir/rkhunter-status.hook"
-	cp "$ASSETS/rkhunter-status.hook" "$file" || x "failed to write: $file"
+	cp "$ASSETS/rkhunter-status.hook" "$file" || _fail "failed to write: $file"
 	_show "$file"
-ksat; fi
+fi
 
 # etckeeper commit
-if $ETCKEEPER && is-task ETCKEEPER DONE; then
+if $ETCKEEPER && _is-task ETCKEEPER DONE; then
 	etckeeper unclean && etckeeper commit "[$LABEL] commit @ $(date +%F)"
 fi
 
 # rkhunter propupd
-if $RKHUNTER && task RKH_PROPUPD -d RKHUNTER; then
-	rkhunter --config-check || x "rkhunter: config error"
-	rkhunter --propupd --report-warnings-only || x "rkhunter: propupd error"
-ksat; fi
-
-
-if ! $OPT_LIST; then
-	echo
-	echo "all done."
-	echo
-	echo "if you are in chroot, type:"
-	echo "exit; umount -R /mnt"
-	echo
+if $RKHUNTER && _task RKH_PROPUPD -d RKHUNTER; then
+	rkhunter --config-check || _fail "rkhunter: config error"
+	rkhunter --propupd --report-warnings-only || _fail "rkhunter: propupd error"
 fi
+
+
+cat <<- EOF
+
+all done.
+
+if you are in chroot, type:
+exit; umount -R /mnt
+
+EOF
+
